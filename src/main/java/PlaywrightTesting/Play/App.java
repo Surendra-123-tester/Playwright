d74a931b6a739cb9de49282b40dc56ae7d640e24
Surendra <surendravk05@gmail.com>
2026-06-24 18:52:27 +0530

    Author changed

---
+++ src/main/java/PlaywrightTesting/Play/App.java
@@ -0,0 +1,13 @@
+package PlaywrightTesting.Play;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
